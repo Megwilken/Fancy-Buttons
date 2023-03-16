021079f42811c22ef6a2fe9a443f84cefbf7c461
Meg Wilken <102841984+Megwilken@users.noreply.github.com>
2023-03-15 22:26:13 -0600

    modify state in components

--- src/components/TextRepeaterButton.js
+++ src/components/TextRepeaterButton.js
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 
 function TextRepeaterButton() {
-  const [repetitions, setRepititions] = useState(1);
+  const [repetitions, setRepetitions] = useState(1);
+
   const textArray = [];
   for (let i = 0; i < repetitions; i++) {
     textArray.push(<span key={i}>I like this text</span>);
   }
+
+  const handleClick = () => {
+    setRepetitions(repetitions + 1);
+  }
+
   return (
-    <button className="TextRepeaterButton">
-      <span>I like this text</span>
+    <button onClick={handleClick} className="TextRepeaterButton">
+      {textArray}
     </button>
   );
 }
